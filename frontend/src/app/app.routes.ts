41c14bcbb7d6cb674287bd30134af9458b49e05b
vectoren <cieslabartek721@gmail.com>
2026-03-05 11:28:41 +0100

    Updated routes, added home page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { DashboardComponent } from './features/dashboard/dashboard.component';
 import { ErrorPageComponent } from './features/error-page/error-page.component';
 import { TrainListComponent } from './features/train-list/train-list.component';
 import { MapViewComponent } from './features/map-view/map-view.component';
+import { HomePageComponent } from './features/home-page/home-page.component';
 
 export const routes: Routes = [
-    {path: '', component: DashboardComponent},
+    {path: '', component: HomePageComponent},
+    {path: 'dashboard', component: DashboardComponent},
     {path: 'train-list', component: TrainListComponent},
     {path: 'map-view', component: MapViewComponent},
     {path: '**', component: ErrorPageComponent}
