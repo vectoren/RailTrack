aad5a1b18d28fef0f1f06b49a6e2995eb89b2bed
vectoren <cieslabartek721@gmail.com>
2026-03-09 11:54:37 +0100

    connected modules with typeorm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,15 +5,28 @@ import { TrainsModule } from './trains/trains.module';
 import { RoutesModule } from './routes/routes.module';
 import { AlertsModule } from './alerts/alerts.module';
 import { ReportsModule } from './reports/reports.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SeedService } from './database/seed.service';
+import { TrainRoute } from './routes/route.entity';
+import { Alert } from './alerts/alert.entity';
+import { Train } from './trains/train.entity';
+import { Report } from './reports/report.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([Train, Alert, Report, TrainRoute]),
     TrainsModule,
-    RoutesModule,
     AlertsModule,
-    ReportsModule
+    ReportsModule,
+    RoutesModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, SeedService],
 })
-export class AppModule {}
+export class AppModule { }
